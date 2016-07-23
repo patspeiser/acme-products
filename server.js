5adb59bdb96d45364eb8920e05c6cc2b83b145b0
pspeiser <pat@patspeiser.com>
2016-07-23 16:17:33 -0400

    bunch of changes

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+var swig = require('swig');
+var routes = require('./js/routes');
+//disable caching
+swig.setDefaults( { cache: false } );
+
+//configure express to use swig. 
+app.engine('html', swig.renderFile);
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
+
+//static route
+app.use(express.static(__dirname + '/node_modules/'));
+app.use('/', routes);
+
+//port to listen on after server starts
+app.listen(5000);
